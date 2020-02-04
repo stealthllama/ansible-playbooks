2346e7d43a30f41bc5259b8ac0bfa03437d9c426
Robert Hagen <rdh@stealthllama.org>
2020-02-04 18:43:43 -0500

    Changed stuff

--- library/panos_devices.py
+++ library/panos_devices.py
@@ -25,21 +25,9 @@ version_added: "2.7"
 requirements:
     - pan-python
     - pandevice
+extends_documentation_fragment:
+    - panos.transitional_provider
 options:
-    ip_address:
-        description:
-            - IP address (or hostname) of PAN-OS device
-        required: true
-    username:
-        description:
-            - username for authentication
-        default: "admin"
-    password:
-        description:
-            - password for authentication
-    api_key:
-        description:
-            - API key to be used instead of I(username) and I(password).
     only_connected:
         description:
             - only list the firewalls that are connected to Panorama.
@@ -48,17 +36,15 @@ options:
 
 EXAMPLES = '''
 # Check Panorama for all managed devices
-- name: check if ready
-  panos_check:
-    ip_address: '{{ panorama_hostname }}'
-    username: '{{ panorama_username }}'
-    password: '{{ panorama_password }}'
+- name: show Panorama devices
+  panos_devices:
+    provider: '{{ provider }}'
+
 
 # Check Panorama for only connected devices
-- name: check if ready
-  panos_check:
-    ip_address: '{{ panorama_hostname }}'
-    api_key: '{{ panorama_key }}'
+- name: show Panorama connected devices
+  panos_devices:
+    provider: '{{ provider }}'
     only_connected: True
 '''
 
@@ -72,72 +58,46 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.basic import get_exception
+from ansible.module_utils.network.panos.panos import get_connection
 
 try:
     from pandevice.base import PanDevice
     from pandevice.panorama import Panorama
     from pandevice.firewall import Firewall
     from pandevice.errors import PanDeviceError
-
-    HAS_LIB = True
 except ImportError:
-    HAS_LIB = False
+    pass
 
 
 def main():
-    # Initialize the module vars
-    argument_spec = dict(
-        ip_address=dict(required=True),
-        username=dict(default='admin'),
-        password=dict(no_log=True),
-        api_key=dict(no_log=True),
-        only_connected=dict(default=False)
+    helper = get_connection(
+        argument_spec = dict(
+            only_connected=dict(default=False)
     )
+
     module = AnsibleModule(
-        argument_spec=argument_spec,
+        argument_spec=helper.argument_spec,
         supports_check_mode=False,
-        mutually_exclusive=[
-            ['api_key', 'password']
-        ]
+        required_one_of=helper.required_one_of,
     )
-    auth = (
-        module.params["ip_address"],
-        module.params['username'],
-        module.params["password"],
-        module.params["api_key"]
-    )
-    only_connected = module.params["only_connected"]
 
-    # Fail if the requirements are not installed
-    if not HAS_LIB:
-        module.fail_json(msg='pan-python and pandevice are required for this module')
 
-    # Open the connection to the PAN-OS device
-    device = None
-    try:
-        device = PanDevice.create_from_device(*auth)
-    except PanDeviceError:
-        e = get_exception()
-        module.fail_json(msg=e.message)
+    parent = helper.get_pandevice_parent(module)
+
 
     # Ensure we're not connected to a firewall
-    if isinstance(device, Firewall):
+    if isinstance(parent, Firewall):
         module.fail_json(msg='This module is only supported on Panorama instances')
 
-    # Set our change flag
-    changed = False
-
     try:
-        devlist = device.refresh_devices(only_connected=only_connected, include_device_groups=False)
-    except PanDeviceError:
-        e = get_exception()
-        module.fail_json(msg=e.message)
+        devlist = parent.refresh_devices(only_connected=only_connected, include_device_groups=False)
+    except PanDeviceError as e:
+        module.fail_json(msg='Failed to restart: {0}'.format(e))
     else:
         devices = []
         for x in devlist:
             devices.append(x.serial)
-        module.exit_json(changed=changed, devices=devices)
+        module.exit_json(changed=False, devices=devices)
 
 
 if __name__ == '__main__':
